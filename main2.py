dde5c97f28c92f787a9dd4fdabb83fa0551d29b1
Mitsuru Kusumoto <kusumoto.mitsuru@gmail.com>
2021-07-24 01:24:02 +0900

    potential

--- main2.py
+++ main2.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 from scipy.spatial import Delaunay
 
 
-def create_sphere_surface_mesh(n_lat, n_lon):
+def create_sphere_surface_mesh(n_lon, n_lat):
     """
     球面をメッシュによって近似し、三角化を施した結果を返す
 
     返り値は (メッシュ数, 3角形頂点, xyz) の ndarray
     """
-    u = np.linspace(0, 2 * np.pi, n_lat + 1)
-    v = np.linspace(-1, 1, n_lon + 1)
+    u = np.linspace(0, 2 * np.pi, n_lon + 1)
+    v = np.linspace(-1, 1, n_lat + 1)
     u, v = np.meshgrid(u, v)
     u = u.flatten()
     v = v.flatten()
@@ -39,6 +39,15 @@ def create_sphere_surface_mesh(n_lat, n_lon):
     return mesh
 
 
+def area(mat):
+    """
+    三角形の面積
+    """
+    a = mat[1] - mat[0]
+    b = mat[2] - mat[0]
+    return np.sqrt(np.dot(a, a) * np.dot(b, b) - np.dot(a, b) ** 2) / 2
+
+
 def sample_triangle_points(mat, n_samples):
     """
     三角形内の点をランダムサンプリングする
@@ -56,30 +65,85 @@ def sample_triangle_points(mat, n_samples):
     return mat[0] + u * (mat[1] - mat[0]) + v * (mat[2] - mat[0])
 
 
-def area(mat):
+class Solver:
     """
-    三角形の面積
+    与えられた導体形状と外部電場から電荷分布を求める
     """
-    a = mat[1] - mat[0]
-    b = mat[2] - mat[0]
-    return np.sqrt(np.dot(a, a) * np.dot(b, b) - np.dot(a, b) ** 2) / 2
 
+    def __init__(self, mesh, pot_fun, eps0=1 / (4 * np.pi)):
+        self.mesh = mesh
+        self.pot_fun = pot_fun
+        self.eps0 = eps0
+        self.qs = None
 
-def approx_surface_potential(mat, eps0=1/(4*np.pi), n_iter=1000):
-    """
-    xyz空間中の三角形に一様に電荷が分布しているとき、その重心のポテンシャルをモンテカルロ計算する。
+    def _prepare(self):
+        """
+        準備計算
+        """
+        self.n = self.mesh.shape[0]
+        # 重心
+        self.gs = self.mesh.mean(axis=1)
+        # 表面での単位電荷量に対するポテンシャル
+        self.pot_surface = np.array([self.approx_surface_potential(mat)
+                                     for mat in self.mesh])
 
-    ここでは三角形内の総電荷量は単位量と仮定する。
-    """
-    ps = sample_triangle_points(mat, n_iter)
-    gs = np.mean(mat, axis=0)
+    def approx_surface_potential(self, mat, n_samples=1000):
+        """
+        xyz空間中の三角形に一様に電荷が分布しているとき、その重心のポテンシャルをモンテカルロ計算する。
 
-    # print(gs)
-    # print(np.linalg.norm(ps - gs, axis=1))
+        ここでは三角形内の総電荷量は単位量と仮定する。
+        """
+        ps = sample_triangle_points(mat, n_samples)
+        gs = np.mean(mat, axis=0)
 
-    integ_approx = np.mean(1 / np.linalg.norm(ps - gs, axis=1))
-    # 面積の項は積分近似と電荷密度の項で打ち消し合うのでここでは現れないことに注意
-    return integ_approx / (4 * np.pi * eps0)
+        integ_approx = np.mean(1 / np.linalg.norm(ps - gs, axis=1))
+        # 面積の項は積分近似と電荷密度の項で打ち消し合うのでここでは現れないことに注意
+        return integ_approx / (4 * np.pi * self.eps0)
+
+    def _solve_core(self, pots):
+        """
+        ポテンシャル条件を満たす荷量を計算する
+        """
+
+        # M[i, j] := 位置 j に単位量の電荷があるときの位置 i のポテンシャル
+        def distmat2(v):
+            xx, yy = np.meshgrid(v, v)
+            return (xx - yy) ** 2
+
+        d = np.sqrt(distmat2(self.gs[:, 0]) +
+                    distmat2(self.gs[:, 1]) + distmat2(self.gs[:, 2]))
+        for i in range(self.n):
+            d[i, i] = 1e-18  # 警告が鬱陶しいので非ゼロにしておく
+        M = 1 / (4 * np.pi * self.eps0 * d)
+        for i in range(self.n):
+            M[i, i] = self.pot_surface[i]
+
+        # TODO: 本当は非対角成分で対角より大きい値があったら潰すべきだが、あまりなさそうなので飛ばす
+        self.qs = np.linalg.solve(M, pots)
+        print(self.qs.sum())
+
+    def solve(self, pot0=0):
+        """
+        電荷分布を計算する
+
+        Args:
+            pot0: 導体表面でのポテンシャル
+        """
+        self._prepare()
+
+        pots = np.array([self.pot_fun(g) for g in self.gs])
+        self._solve_core(pot0 - pots)
+
+    def calc_pot(self, p):
+        """
+        与えられた点のポテンシャルを計算する (外部電場の影響を考慮して計算する)
+        """
+        pot = self.pot_fun(p)
+        for i in range(self.n):
+            r = np.linalg.norm(p - self.gs[i])
+            pot += self.qs[i] * min(1 / (4 * np.pi * self.eps0 * r),
+                                    self.pot_surface[i])
+        return pot
 
 
 def get_pot_of_const_ef(e0):
@@ -91,53 +155,28 @@ def get_pot_of_const_ef(e0):
     return pot
 
 
-def solve_core(mesh, pot0, eps0=1/(4*np.pi)):
-    """
-    ポテンシャル条件を満たす荷量を計算する
-    """
-
-    gs = mesh.mean(axis=1)
-    n = mesh.shape[0]
-
-    # 表面ポテンシャルを計算する
-    pot_surface = np.array([approx_surface_potential(mat) for mat in mesh])
-
-    # M[i, j] := 位置 j に単位量の電荷があるときの位置 i のポテンシャル
-    def distmat2(v):
-        xx, yy = np.meshgrid(v, v)
-        return (xx - yy) ** 2
-
-    d = np.sqrt(distmat2(gs[:, 0]) + distmat2(gs[:, 1]) + distmat2(gs[:, 2]))
-    print(d)
-    for i in range(n):
-        d[i, i] = 1e-18  # 警告が鬱陶しいので非ゼロにしておく
-    M = 1 / (4 * np.pi * eps0 * d)
-    for i in range(n):
-        M[i, i] = pot_surface[i]
-
-    # TODO: 本当は非対角成分で対角より大きい値があったら潰すべきだが、あまりなさそうなので飛ばす
-    qs = np.linalg.solve(M, pot0)
-    As = np.array([area(mat) for mat in mesh])
-    print(np.sum(qs))
-    return qs
-
-
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--n_ec', type=int, default=40,
-                        help='電荷の配置数')
-    parser.add_argument('--pixel_len', type=float, default=1,
-                        help='1ピクセルが表す長さ [m]')
+    parser.add_argument('--n_lon', type=int, default=20,
+                        help='メッシュの分解能 (経度方向)')
+    parser.add_argument('--n_lat', type=int, default=20,
+                        help='メッシュの分解能 (緯度方向)')
     parser.add_argument('--eps0', type=float, default=1,
                         help='真空の誘電率 [Fm-1]')
     args = parser.parse_args()
 
+    mesh = create_sphere_surface_mesh(args.n_lon, args.n_lat)
+    pot_fun = get_pot_of_const_ef(np.array([0.3, 0, 0]))
+    solver = Solver(mesh, pot_fun)
+
     mat = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0]])
-    print(approx_surface_potential(mat))
+    print(solver.approx_surface_potential(mat))
 
-    mesh = create_sphere_surface_mesh(20, 20)
+    # solver._solve_core(np.ones(mesh.shape[0]))
+    solver.solve()
 
-    solve_core(mesh, np.ones(mesh.shape[0]))
+    for i in range(-20, 21):
+        print(i / 10, solver.calc_pot(np.array([i / 10, 0, 0.86])))
 
 
 if __name__ == "__main__":
