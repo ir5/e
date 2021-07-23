14e65a9d754927c2edb4dd5f6344cd9832ed9b1b
Mitsuru Kusumoto <kusumoto.mitsuru@gmail.com>
2021-07-23 20:31:48 +0900

    2d is broken

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ def main():
                         help='電荷の配置数')
     parser.add_argument('--pixel_len', type=float, default=1,
                         help='1ピクセルが表す長さ [m]')
-    parser.add_argument('--thickness', type=float, default=1,
-                        help='導体板の厚さ [m]')
     parser.add_argument('--eps0', type=float, default=1,
                         help='真空の誘電率 [Fm-1]')
     args = parser.parse_args()
@@ -69,6 +67,7 @@ def main():
     ec_poses = np.array(ec_poses)
     ec_normals = np.array(ec_normals)
 
+    """
     qs = np.zeros(n_ec)  # 該当領域の総電荷
     A = args.thickness * total_len / n_ec  # 該当領域の面積
     R = np.zeros((2, n_ec, n_ec))  # クーロン力の重み行列(成分ごと)
@@ -94,7 +93,42 @@ def main():
     b = -np.dot(E_const, ec_normals.T)
 
     qs = np.linalg.solve(M, b)
+    """
 
+    # ポテンシャルベースで電荷量を求める
+    E_const = np.array([0.1, 0])
+    M = np.zeros((n_ec, n_ec))
+    a = total_len / n_ec / 2
+    inner_poses = []
+    for i in range(n_ec):
+        inner_poses.append(ec_poses[i] + 4 * a * ec_normals[i])
+
+    for i in range(n_ec):
+        for j in range(n_ec):
+            """
+            pi = inner_poses[i]
+            pj = ec_poses[j]
+            r = np.linalg.norm(pi - pj) * args.pixel_len
+            M[i, j] = 1 / (4 * np.pi * args.eps0 * r)
+            """
+            if i != j:
+                pi = ec_poses[i]
+                pj = ec_poses[j]
+                r = np.linalg.norm(pi - pj) * args.pixel_len
+                M[i, j] = 1 / (4 * np.pi * args.eps0 * r)
+            else:
+                M[i, j] = 1 / (2 * np.pi * args.eps0 * a)
+
+    # b = np.array([np.dot(E_const, inner_poses[i]) for i in range(n_ec)])
+    b = np.array([np.dot(E_const, ec_poses[i]) for i in range(n_ec)])
+    qs = np.linalg.solve(M, -b)
+
+    print(np.dot(M, qs) + b)
+    return
+
+    A = 4 * a ** 2
+
+    """
     # 電場計算
     E_q = np.dot(R, qs).T  # (n_ec, 2)
     E = E_q + E_const
@@ -103,16 +137,61 @@ def main():
     q_flow = np.array([np.dot(E[i], ec_normals[i]) for i in range(n_ec)])
 
     print(q_flow)
+    """
 
     # 電荷密度
     q_densities = qs / A
 
     print(q_densities)
 
-    # 電場計算
+    # ポテンシャル計算
     height, width, _ = img.shape
     nx = 20
     ny = 20
+    pot_poses = []
+    pots = []
+    for ix in range(nx):
+        for iy in range(ny):
+            x = (ix + 0.5) * width / nx
+            y = (iy + 0.5) * height / ny
+            p = np.array([x, y])
+
+            pot = np.dot(E_const, p)
+            for i in range(n_ec):
+                r = np.linalg.norm(p - ec_poses[i]) * args.pixel_len
+                pot += qs[i] / (4 * np.pi * args.eps0 * max(a / 2, r))
+
+            pot_poses.append(p)
+            pots.append(pot)
+
+    q_lim = np.max(np.abs(q_densities))
+    pot_lim = np.max(np.abs(pots))
+
+    def to_pt(pos, dx=0, dy=0):
+        return (int(pos[0]) + dx, int(pos[1]) + dy)
+
+    for ec_pos, q_density in zip(ec_poses, q_densities):
+        intensity = int(191 * abs(q_density) / q_lim) + 64
+        if q_density < 0:
+            color = (intensity, 64, 64)
+        else:
+            color = (64, 64, intensity)
+
+        cv2.line(img, to_pt(ec_pos, dx=3), to_pt(ec_pos, dx=-3), color)
+        cv2.line(img, to_pt(ec_pos, dy=3), to_pt(ec_pos, dy=-3), color)
+
+    for pos, pot in zip(pot_poses, pots):
+        v = abs(pot) * 8 / pot_lim + 1
+        cv2.circle(img, to_pt(pos), int(v), (0, 255, 0))
+
+    cv2.imwrite("a.png", img)
+
+    return
+
+    # 電場計算
+    height, width, _ = img.shape
+    nx = 1
+    ny = 1
     ef_poses = []
     ef_vec = []
     for ix in range(nx):
